ed205773644b8ef16ab1e69d8d38205421201de3
Zoltan Ray <zoltanray@gmail.com>
2020-02-28 12:26:36 +0000

    brought in rgb > hsl function to get rollup working

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,15 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import url from "@rollup/plugin-url";
 import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import autoExternal from "rollup-plugin-auto-external";
-import resolve from "rollup-plugin-node-resolve";
-import url from "rollup-plugin-url";
 
 import pkg from "./package.json";
 
 export default {
   input: "src/index.js",
-  external: ["uuid", "@alchemyalcove/rgb-to-hsl"],
-  globals: { uuid: "uuid", "@alchemyalcove/rgb-to-hsl": "Convert" },
+  externals: ["uuid"],
+  globals: { react: "React" },
   output: [
     {
       file: pkg.main,
@@ -24,20 +23,17 @@ export default {
     }
   ],
   plugins: [
-    external(),
-    url({ exclude: ["**/*.svg"] }),
+    resolve(),
+    commonjs({
+      include: "node_modules/**",
+      namedExports: {
+        react: ["useState", "useRef", "useEffect"]
+      }
+    }),
+    peerDepsExternal(),
     babel({
       exclude: "node_modules/**"
     }),
-    resolve({
-      modulesOnly: true
-    }),
-    commonjs({
-      namedExports: {
-        uuid: ["uuid"],
-        "@alchemyalcove/rgb-to-hsl": ["Convert"]
-      }
-    }),
-    autoExternal()
+    url({ exclude: ["**/*.svg"] })
   ]
 };
