8e2f68ffdde9e5ea5bcb3afce0f9b46b45a6e331
Zoltan Ray <zoltanray@gmail.com>
2020-02-28 15:30:34 +0000

    moved conversion function to import

---
+++ src/convertRgbToHsl.js
@@ -0,0 +1,41 @@
+const convert = rgb => {
+  var r = rgb[0] / 255;
+  var g = rgb[1] / 255;
+  var b = rgb[2] / 255;
+  var max = Math.max(r, g, b);
+  var min = Math.min(r, g, b);
+  var delta = max - min;
+  var h = void 0;
+  var s = void 0;
+  var l = void 0;
+
+  if (max === min) {
+    h = 0;
+  } else if (r === max) {
+    h = (g - b) / delta;
+  } else if (g === max) {
+    h = 2 + (b - r) / delta;
+  } else if (b === max) {
+    h = 4 + (r - g) / delta;
+  }
+
+  h = Math.min(h * 60, 360);
+
+  if (h < 0) {
+    h += 360;
+  }
+
+  l = (min + max) / 2;
+
+  if (max === min) {
+    s = 0;
+  } else if (l <= 0.5) {
+    s = delta / (max + min);
+  } else {
+    s = delta / (2 - max - min);
+  }
+
+  return [h, s * 100, l * 100];
+};
+
+export default convert;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
-
-const RGBtoHSL = rgb => {
-  var r = rgb[0] / 255;
-  var g = rgb[1] / 255;
-  var b = rgb[2] / 255;
-  var max = Math.max(r, g, b);
-  var min = Math.min(r, g, b);
-  var delta = max - min;
-  var h = void 0;
-  var s = void 0;
-  var l = void 0;
-
-  if (max === min) {
-    h = 0;
-  } else if (r === max) {
-    h = (g - b) / delta;
-  } else if (g === max) {
-    h = 2 + (b - r) / delta;
-  } else if (b === max) {
-    h = 4 + (r - g) / delta;
-  }
-
-  h = Math.min(h * 60, 360);
-
-  if (h < 0) {
-    h += 360;
-  }
-
-  l = (min + max) / 2;
-
-  if (max === min) {
-    s = 0;
-  } else if (l <= 0.5) {
-    s = delta / (max + min);
-  } else {
-    s = delta / (2 - max - min);
-  }
-
-  return [h, s * 100, l * 100];
-};
+import convert from "./convertRgbToHsl";
 
 const useGradientEffector = ({
   dimensions: { width, height } = {},
@@ -105,7 +66,7 @@ const useGradientEffector = ({
           );
 
           // Convert to HSL
-          const hslValue = RGBtoHSL([
+          const hslValue = convert([
             newGradientValue.data[0],
             newGradientValue.data[1],
             newGradientValue.data[2]
